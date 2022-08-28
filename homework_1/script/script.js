b89ae423e5232447941e3accaefc41b2d648fdfd
Anastasiia Myelnykova <slonotop2103@gmail.com>
2022-08-28 17:50:15 +0300

    homework_1_js

---
+++ homework_1/script/script.js
@@ -0,0 +1,19 @@
+"use strict"
+//Task 1
+let admin;
+let name = "Nastya";
+admin = name;
+
+
+//Task 2
+let days = 8;
+let second = days * 60;
+console.log(`${second} second`);
+
+
+//Task 3
+let userValue = prompt("Напишіть щось і побачите це в консолі");
+console.log(userValue);
+
+
+
